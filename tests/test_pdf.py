d90bfb5f8864eabe46366ed463abf9e27f7f0843
Tamas Gal <tgal@km3net.de>
2020-02-26 19:09:10 +0100

    Add tests

---
+++ tests/test_pdf.py
@@ -0,0 +1,14 @@
+import os
+import unittest
+import jppy
+
+PDFS = os.path.expandvars("$JPP_DIR/data/J%p.dat")
+
+class TestMuonPDF(unittest.TestCase):
+    def test_pdf(self):
+        muon_pdf = jppy.pdf.JMuonPDF(PDFS, 0)
+        result = muon_pdf.calculate(10, 5, 0, 0, 23)
+        self.assertAlmostEqual(0.001535784, result.f)
+        self.assertAlmostEqual(-2.22809691e-05, result.fp)
+        self.assertAlmostEqual(0.024902763, result.v)
+        self.assertAlmostEqual(0.116492968, result.V)
