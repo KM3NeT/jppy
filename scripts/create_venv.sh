f78b1533e759f8e852eaee72ecf3290a6e77b5cf
Tamas Gal <tgal@km3net.de>
2020-02-28 18:33:07 +0100

    Add script to create and manage the virtualenv

---
+++ scripts/create_venv.sh
@@ -0,0 +1,8 @@
+#!/bin/bin/env bash
+if [ ! -d "venv" ]; then
+    echo "Creating a fresh virtualenv..."
+    pip install -U pip setuptools wheel
+    python -m venv venv
+else
+    echo "Virtualenv already created."
+fi
