cc1705f5c72126b07910a584ebf2f649d0395363
Tamas Gal <tgal@km3net.de>
2020-02-28 18:32:38 +0100

    Add script to retrieve PDFs

--- scripts/get_pdfs.sh
+++ scripts/get_pdfs.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bin/env bash
 if [ ! -d "pdfs" ]; then
+    echo "Retrieving PDFs..."
     mkdir pdfs
     cd pdfs
     for i in {1..6}; do
         wget "http://pi1139.physik.uni-erlangen.de/data/latest/J${i}p.dat"
     done
-    cd ..
+else
+    echo "PDFs already downloaded."
 fi
