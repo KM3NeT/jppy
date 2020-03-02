043954e6d66571c72a30c7c43bc53ad22e83e80f
Tamas Gal <tgal@km3net.de>
2020-03-02 17:54:20 +0100

    Update shower values

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -15,8 +15,8 @@ class TestMuonPDF(unittest.TestCase):
 class TestShowerPDF(unittest.TestCase):
     def test_pdf(self):
         shower_pdf = jppy.pdf.JShowerPDF(PDFS, 0)
-        result = shower_pdf.calculate(10, 5, 0, 0, 23)
-        self.assertAlmostEqual(0.001535784, result.f)
-        self.assertAlmostEqual(-2.22809691e-05, result.fp)
-        self.assertAlmostEqual(0.024902763, result.v)
-        self.assertAlmostEqual(0.116492968, result.V)
+        result = shower_pdf.calculate(100, 10, 0.1, 0.2, 0.3, 6)
+        self.assertAlmostEqual(0.001612553295068934, result.f)
+        self.assertAlmostEqual(4.000285659029551e-05, result.fp)
+        self.assertAlmostEqual(0.010999553987301543, result.v)
+        self.assertAlmostEqual(0.1527856994106781, result.V)
