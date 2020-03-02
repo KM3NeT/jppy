db319aa8544de03b4af3a277c41ea53927f3c314
Tamas Gal <tgal@km3net.de>
2020-03-02 18:05:04 +0100

    Add shower pdf

--- scripts/get_pdfs.sh
+++ scripts/get_pdfs.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
+export URL="http://pi1139.physik.uni-erlangen.de/data/latest"
 if [ ! -d "pdfs" ]; then
     echo "Retrieving PDFs..."
     mkdir pdfs
     cd pdfs
     for i in {1..6}; do
-        wget "http://pi1139.physik.uni-erlangen.de/data/latest/J${i}p.dat"
+        wget "$URL/J${i}p.dat"
+    done
+    for i in {13..14}; do
+        wget "$URL/J${i}p.dat"
     done
 else
     echo "PDFs already downloaded."
