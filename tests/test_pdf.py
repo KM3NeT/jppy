fce804e3e22bba327f6d4f07f2536060706a4c6d
Tamas Gal <himself@tamasgal.com>
2022-09-19 22:11:54 +0200

    Adapt tests to new PDF values

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -7,7 +7,7 @@ class TestMuonPDF(unittest.TestCase):
     def test_pdf(self):
         muon_pdf = jppy.pdf.JMuonPDF(PDFS, 0)
         result = muon_pdf.calculate(10, 5, 0, 0, 23)
-        self.assertAlmostEqual(0.001545692, result.f)
+        self.assertAlmostEqual(0.00067762789, result.f)
         self.assertAlmostEqual(-1.220889709e-05, result.fp)
         self.assertAlmostEqual(0.022764524, result.v)
         self.assertAlmostEqual(0.115814468, result.V)
@@ -16,7 +16,7 @@ class TestShowerPDF(unittest.TestCase):
     def test_pdf(self):
         shower_pdf = jppy.pdf.JShowerPDF(PDFS, 0)
         result = shower_pdf.calculate(100, 10, 0.1, 0.2, 0.3, 6)
-        self.assertAlmostEqual(0.001383343, result.f)
+        self.assertAlmostEqual(0.0013937540, result.f)
         self.assertAlmostEqual(5.091930537e-05, result.fp)
         self.assertAlmostEqual(0.010475308, result.v)
         self.assertAlmostEqual(0.149635554, result.V)
--- tests/test_pdf_evaluator.py
+++ tests/test_pdf_evaluator.py
@@ -8,7 +8,7 @@ class TestMuonPDFEvaluator(unittest.TestCase):
         E, t0, t_obs, D, cd, theta, phi = [1e3, 56, 292, 50, 0.7, 1.57, 3.14]
         muon_pdf = jppy.pdf_evaluator.MuonPDF(PDFS, E, t0)
         result = muon_pdf.evaluate(D, cd, theta, phi, t_obs)
-        self.assertAlmostEqual( 0.003644475, result.f)
+        self.assertAlmostEqual( 0.00388579021, result.f)
         self.assertAlmostEqual(-0.000715558, result.fp)
         self.assertAlmostEqual( 0.033748905, result.v)
         self.assertAlmostEqual( 0.097171157, result.V)
@@ -18,7 +18,7 @@ class TestShowerPDFEvaluator(unittest.TestCase):
         E, t0, t_obs, D, cd, theta, phi = [50, 198, 226, 5, 0.6, 0.5, 0.4]
         shower_pdf = jppy.pdf_evaluator.ShowerPDF(PDFS, E, t0)
         result = shower_pdf.evaluate(D, cd, theta, phi, t_obs)
-        self.assertAlmostEqual(0.006204617, result.f)
+        self.assertAlmostEqual(0.006247665, result.f)
         self.assertAlmostEqual(0.000641669, result.fp)
         self.assertAlmostEqual(0.013960066, result.v)
         self.assertAlmostEqual(0.296589983, result.V)
