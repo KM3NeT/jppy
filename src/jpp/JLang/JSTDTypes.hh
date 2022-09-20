5247f02ff3ca6c04549f9c0b9ee59b9c1ba2ee35
Tamas Gal <tgal@km3net.de>
2022-09-20 07:15:38 +0000

    Resolve "Installing Jppy does not work on my MacOS system"

--- src/jpp/JLang/JSTDTypes.hh
+++ src/jpp/JLang/JSTDTypes.hh
@@ -8,22 +8,30 @@
  * \author mdejong
  */
 
-namespace std {
-  template<class JElement_t, class JAllocator_t>                                    class vector;
-  template<class JElement_t, class JComparator_t, class JAllocator_t>               class set;
-  template<class JElement_t, class JComparator_t, class JAllocator_t>               class multiset;
-  template<class JFirst_t, class JSecond_t>                                         class pair;
-  template<class JKey_t, class JValue_t, class JComparator_t, class JAllocator_t>   class map;
-  template<class JKey_t, class JValue_t, class JComparator_t, class JAllocator_t>   class multimap;
-  template<class Category, class T, class Distance, class Pointer, class Reference> struct iterator;
-#ifdef _GLIBCXX_USE_CXX11_ABI
-  inline namespace __cxx11 {
-    template<class JElement_t, class JAllocator_t>                                  class list;
-  }
-#else
-  template<class JElement_t, class JAllocator_t>                                    class list;
-#endif 
-}
+#include <vector>
+#include <set>
+#include <utility>
+#include <map>
+#include <list>
+#include <iterator>
+
+// TODO: this gives an ambigous error on macOS with std::pair
+// namespace std {
+//   template<class JElement_t, class JAllocator_t>                                    class vector;
+//   template<class JElement_t, class JComparator_t, class JAllocator_t>               class set;
+//   template<class JElement_t, class JComparator_t, class JAllocator_t>               class multiset;
+//   template<class JFirst_t, class JSecond_t>                                         struct pair;
+//   template<class JKey_t, class JValue_t, class JComparator_t, class JAllocator_t>   class map;
+//   template<class JKey_t, class JValue_t, class JComparator_t, class JAllocator_t>   class multimap;
+//   template<class Category, class T, class Distance, class Pointer, class Reference> struct iterator;
+// #ifdef _GLIBCXX_USE_CXX11_ABI
+//   inline namespace __cxx11 {
+//     template<class JElement_t, class JAllocator_t>                                  class list;
+//   }
+// #else
+//   template<class JElement_t, class JAllocator_t>                                    class list;
+// #endif
+// }
 
 #endif
 
--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -7,16 +7,16 @@ class TestMuonPDF(unittest.TestCase):
     def test_pdf(self):
         muon_pdf = jppy.pdf.JMuonPDF(PDFS, 0)
         result = muon_pdf.calculate(10, 5, 0, 0, 23)
-        self.assertAlmostEqual(0.001545692, result.f)
-        self.assertAlmostEqual(-1.220889709e-05, result.fp)
-        self.assertAlmostEqual(0.022764524, result.v)
-        self.assertAlmostEqual(0.115814468, result.V)
+        self.assertAlmostEqual(0.00067762789, result.f)
+        self.assertAlmostEqual(-1.9661888268460384e-05, result.fp)
+        self.assertAlmostEqual(0.01374949305, result.v)
+        self.assertAlmostEqual(0.053314508, result.V)
 
 class TestShowerPDF(unittest.TestCase):
     def test_pdf(self):
         shower_pdf = jppy.pdf.JShowerPDF(PDFS, 0)
         result = shower_pdf.calculate(100, 10, 0.1, 0.2, 0.3, 6)
-        self.assertAlmostEqual(0.001383343, result.f)
-        self.assertAlmostEqual(5.091930537e-05, result.fp)
-        self.assertAlmostEqual(0.010475308, result.v)
-        self.assertAlmostEqual(0.149635554, result.V)
+        self.assertAlmostEqual(0.0013937540, result.f)
+        self.assertAlmostEqual(5.1614638414144165e-05, result.fp)
+        self.assertAlmostEqual(0.01054815545, result.v)
+        self.assertAlmostEqual(0.150554603, result.V)
